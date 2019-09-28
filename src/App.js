2c76c14e9e20701987edacaf744b9b99e19b9235
fobabett <astronomemusic@gmail.com>
2019-09-27 20:43:22 -0700

    moves CodeMirror into Editor

--- src/App.js
+++ src/App.js
@@ -1,47 +1,25 @@
 import React, { useState } from 'react';
+import { Provider } from 'react-redux';
+
+import store from './store';
+import Player from './components/Player'
+import Editor from './components/Editor'
+
 import './App.css';
-import CodeMirror from 'react-codemirror';
-import "../node_modules/codemirror/lib/codemirror.css";
-import "../node_modules/codemirror/theme/dracula.css";
-import '../node_modules/codemirror/mode/javascript/javascript';
 
 const App = () => {
-  let [code, setCode] = useState()
-  let options = {
-    lineNumbers: true,
-    mode: 'javascript',
-    theme: 'dracula'
-  }
-
-  const moveUpTest = () => {
-    return 1;
-  }
-
-  const onChange = (val) => {
-    setCode(val.replace(/\/\*[\s\S]*?\*\/|([^\\:]|^)\/\/.*$|(<script>)|eval|XMLHttpRequest|document\.write/gm,""))
-  }
-
-  const run = () => {
-    try {
-      var result = (() => { return eval(code)})()
-      console.log(result)
-      //calling moveUpTest in browser: result = 1
-
-    } catch (err) {
-      console.log(err)
-    }
-  }
 
   return (
-    <div className="container">
-      <div className="game-container">
-        <div className="game">Game Here</div>
-        <button onClick={run}>Run</button>
+    <Provider store={store}>
+      <div className="container">
+        <div className="game-container">
+          <div className="game">
+            <Player />
+          </div>
+        </div>
+        <Editor />
       </div>
-      <div className='editor-container'> 
-        <CodeMirror className='editor' options={options} onChange={onChange} />
-      </div>
-    </div>
+    </Provider>
   );
 }
 
---
+++ src/components/Editor.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import CodeMirror from 'react-codemirror';
+import "../../node_modules/codemirror/lib/codemirror.css";
+import "../../node_modules/codemirror/theme/dracula.css";
+import '../../node_modules/codemirror/mode/javascript/javascript';
+
+export const Editor = () => {
+
+  let [code, setCode] = useState()
+  let options = {
+    lineNumbers: true,
+    mode: 'javascript',
+    theme: 'dracula'
+  }
+
+  const onChange = (val) => {
+    setCode(val.replace(/\/\*[\s\S]*?\*\/|([^\\:]|^)\/\/.*$|(<script>)|eval|XMLHttpRequest|document\.write/gm, ""))
+  }
+
+  const run = () => {
+    try {
+      var result = (() => { return eval(code) })()
+      console.log(result)
+
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  return (
+    <div className='editor-container'>
+      <CodeMirror className='editor' options={options} onChange={onChange} />
+      <button onClick={run}>Run</button>
+    </div>
+  )
+}
+
+export default Editor
