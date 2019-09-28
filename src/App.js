c937b63ac251de2dd53b872bc949cd0ca17b7a03
fobabett <astronomemusic@gmail.com>
2019-09-27 18:51:32 -0700

    user's code evaluated on run

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import CodeMirror from 'react-codemirror';
 import "../node_modules/codemirror/lib/codemirror.css";
@@ -6,24 +6,37 @@ import "../node_modules/codemirror/theme/dracula.css";
 import '../node_modules/codemirror/mode/javascript/javascript';
 
 const App = () => {
+  let [code, setCode] = useState()
   let options = {
     lineNumbers: true,
     mode: 'javascript',
     theme: 'dracula'
   }
 
-  useEffect(() => {
-    // CodeMirror.setSize("100%", "100%")
-  }, [])
+  const moveUpTest = () => {
+    return 1;
+  }
 
   const onChange = (val) => {
-    console.log(val)
+    setCode(val.replace(/\/\*[\s\S]*?\*\/|([^\\:]|^)\/\/.*$|(<script>)|eval|XMLHttpRequest|document\.write/gm,""))
+  }
+
+  const run = () => {
+    try {
+      var result = (() => { return eval(code)})()
+      console.log(result)
+      //calling moveUpTest in browser: result = 1
+
+    } catch (err) {
+      console.log(err)
+    }
   }
 
   return (
     <div className="container">
       <div className="game-container">
         <div className="game">Game Here</div>
+        <button onClick={run}>Run</button>
       </div>
       <div className='editor-container'> 
         <CodeMirror className='editor' options={options} onChange={onChange} />
