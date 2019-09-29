f7017899a61910696c4906ccf6f73221495c41c5
Kelli <kelli@gomagames.com>
2019-09-29 16:06:55 -0700

    added title graphic

--- src/screens/Menu.js
+++ src/screens/Menu.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import titleGraphic from '../assets/dungeon-title.png'
 
 export default (props) => {
 
@@ -8,7 +9,7 @@ export default (props) => {
 
   return (
     <div className="title-screen">
-      <h1>[Title Here]</h1>
+      <img className="title-graphic" src={titleGraphic} alt="DungeonJS" />
       <p>Learn JavaScript or test your skills</p>
       <button onClick={lessons} className="button title-button">Lessons</button>
       <div>
