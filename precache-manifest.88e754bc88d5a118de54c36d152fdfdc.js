741f6a30a3e1cf2526396e659409940d2b8668e3
fobabett <astronomemusic@gmail.com>
2020-04-21 17:32:49 -0700

    Updates

---
+++ precache-manifest.88e754bc88d5a118de54c36d152fdfdc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6aa68ab8e307508857529c9460e01f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf6f8135e6ee9ac99eba",
+    "url": "/static/css/2.578505c4.chunk.css"
+  },
+  {
+    "revision": "77f44d60a04b9c3a359a",
+    "url": "/static/css/main.283f2c6c.chunk.css"
+  },
+  {
+    "revision": "bf6f8135e6ee9ac99eba",
+    "url": "/static/js/2.f1061a80.chunk.js"
+  },
+  {
+    "revision": "77f44d60a04b9c3a359a",
+    "url": "/static/js/main.10d51c8f.chunk.js"
+  },
+  {
+    "revision": "6d0c557a05ddc2ba4105",
+    "url": "/static/js/runtime-main.89bebcf1.js"
+  }
+]);
